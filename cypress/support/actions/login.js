244b6ce6d258848f52806b151ec4ed7d1215f23d
Yuri Mielli <yumielllli@gmail.com>
2026-07-28 21:23:20 +0200

    add tempo para cy.visit

--- cypress/support/actions/login.js
+++ cypress/support/actions/login.js
@@ -4,7 +4,9 @@ import users from '../../fixtures/data.json'
 
 Cypress.Commands.add('login', (email, senha) => {
     cy.setCookie('ebacStoreVersion', Cypress.env("ebacStoreVersion"), { domain: 'lojaebac.ebaconline.art.br' })
+    cy.wait(2000)
     cy.visit("/")
+    cy.wait(2000)
     homePage.openMenu('Account')
     loginPage.login(email, senha)
 })
